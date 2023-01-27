feb7dedeef88fab820e9adb6bafa75523d0d16e4
k-hayack884 <hayack885@gmail.com>
2023-01-27 14:14:15 +0900

    ユニット毎でかぶった月を表示する機能を作成

--- app/Packages/Domains/Water/WaterSettingCollection.php
+++ app/Packages/Domains/Water/WaterSettingCollection.php
@@ -5,7 +5,6 @@ namespace App\Packages\Domains\Water;
 use App\Exceptions\NotFoundException;
 use ArrayIterator;
 use Closure;
-use DomainException;
 use Illuminate\Support\Collection;
 use IteratorAggregate;
 use ReturnTypeWillChange;
@@ -31,32 +30,80 @@ class WaterSettingCollection implements IteratorAggregate
      * @param MonthsWaterSetting $waterSetting
      * @return void
      */
-    public function add(MonthsWaterSetting $waterSetting)
+    public function add(MonthsWaterSetting $waterSetting): void
     {
         $this->collection->put($waterSetting->getWaterSettingId()->getId(), $waterSetting);
     }
 
     /**
      * @param WaterSettingId $waterSettingId
-     * @return Closure
+     * @return MonthsWaterSetting
      * @throws NotFoundException
      */
-    public function findById(WaterSettingId $waterSettingId)
+    public function findById(WaterSettingId $waterSettingId): MonthsWaterSetting
     {
         $waterSetting = $this->collection->get($waterSettingId->getId());
+
         if (is_null($waterSetting)) {
-            throw new NotFoundException('選んだ設定が見つかりませんでした (id:' . $waterSettingId->getId() . ')');
+            throw new NotFoundException('選んだ水やり設定IDが見つかりませんでした (id:' . $waterSettingId->getId() . ')');
+        }
+        if (!$waterSetting->getWaterSettingId()->equals($waterSettingId)) {
+            throw new NotFoundException('選んだ水やり設定IDが見つかりませんでした (id:' . $waterSettingId->getId() . ')');
         }
         return $waterSetting;
+    }
+
+    /**
+     * @param int $value
+     * @return Closure|null
+     */
+    public function getValue(int $value): ?Closure
+    {
+        return $this->collection->get($value);
     }
 
     /**
      * @param MonthsWaterSetting $waterSetting
      * @return void
      */
-    public function delete(MonthsWaterSetting $waterSetting):void
+    public function delete(MonthsWaterSetting $waterSetting): void
     {
         $this->collection->forget($waterSetting->getWaterSettingId()->getId());
+    }
+
+    /**
+     * @return WaterSettingCollection
+     */
+    public function duplicationDisplay(): WaterSettingCollection
+    {
+        $waterSettings = $this->toArray();
+        $duplicationSettings = [];
+        foreach ($waterSettings as $waterSetting) {
+            if ($this->duplicationMonthCheck($waterSetting)) {
+                $duplicationSettings[] = $waterSetting;
+            }
+        }
+        return new self($duplicationSettings);
+    }
+
+    /**
+     * @param $waterSetting
+     * @return bool
+     */
+    private function duplicationMonthCheck($waterSetting): bool
+    {
+        $referenceWaterSettings = $this->toArray();
+        foreach ($referenceWaterSettings as $referenceWaterSetting) {
+            if ($referenceWaterSetting === $waterSetting) {
+                continue;
+            }
+            $sumMonthArray = array_merge($referenceWaterSetting->getMonths(), $waterSetting->getMonths());
+            $deleteDuplicationArray = array_unique($sumMonthArray);
+            if (count($sumMonthArray) !== count($deleteDuplicationArray)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     /**
