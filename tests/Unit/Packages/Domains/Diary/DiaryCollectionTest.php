f7990803be317e45064e8efb2ac59bc404cd656c
k-hayack884 <hayack885@gmail.com>
2023-02-17 16:34:12 +0900

    日付によってソートされるように変更

--- tests/Unit/Packages/Domains/Diary/DiaryCollectionTest.php
+++ tests/Unit/Packages/Domains/Diary/DiaryCollectionTest.php
@@ -7,6 +7,7 @@ use App\Packages\Domains\Diary\Diary;
 use App\Packages\Domains\Diary\DiaryCollection;
 use App\Packages\Domains\Diary\DiaryContent;
 use App\Packages\Domains\Diary\DiaryId;
+use Carbon\Carbon;
 use PHPUnit\Framework\TestCase;
 
 class DiaryCollectionTest extends TestCase
@@ -70,6 +71,7 @@ class DiaryCollectionTest extends TestCase
         }
 
     }
+
     public function test_設定を削除すること()
     {
         $diaries = [
@@ -90,5 +92,53 @@ class DiaryCollectionTest extends TestCase
         $diaryCollection->delete($deleteDiary);
         $this->expectException(NotFoundException::class);
         $getDiary = $diaryCollection->findById($diaryId);
+    }
+
+    public function test_オブジェクトの内容が日付順で並び変えられること()
+    {
+        $before1DayDate = Carbon::yesterday();
+        $nowDate = Carbon::now();
+        $after1dayDate = Carbon::tomorrow();
+        $after1HourDate = Carbon::now()->addHours(1);
+
+        $diaries = [
+            new Diary(
+                new DiaryId('983c1092-7a0d-40b0-af6e-30bff5975e31'),
+                new DiaryContent('昨日'),
+                [],
+                $before1DayDate
+            ),
+            new Diary(
+                new DiaryId('333c1092-7a0d-40b0-af6e-30bff5975e31'),
+                new DiaryContent('明日'),
+                [],
+                $after1dayDate
+            ),
+            new Diary(
+                new DiaryId('888c1092-7a0d-40b0-af6e-30bff5975e31'),
+                new DiaryContent('今日'),
+                [],
+                $nowDate
+            ),
+            new Diary(
+                new DiaryId('999c1092-7a0d-40b0-af6e-30bff5975e31'),
+                new DiaryContent('今日＋1時間'),
+                [],
+                $after1HourDate
+            )
+        ];
+        $diaryCollection = new DiaryCollection($diaries);
+        $sortedCollection = $diaryCollection->sortDate();
+        $sortedArray = $sortedCollection->toArray();
+        foreach ($sortedArray as $value) {
+            $results[] = $value->getDiaryContent()->getValue();
+        }
+        $this->assertCount(count($diaries), $sortedCollection);
+        $this->assertSame('明日', $results[0]);
+        $this->assertSame('今日＋1時間', $results[1]);
+        $this->assertSame('今日', $results[2]);
+        $this->assertSame('昨日', $results[3]);
+
+
     }
 }
