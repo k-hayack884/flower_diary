24867a168dc05f5db5eba7c86cde200859a03004
k-hayack884 <hayack885@gmail.com>
2023-03-16 15:40:26 +0900

    vueでテストするひな形づくり

---
+++ resources/js/Pages/Comment.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { Head, Link } from '@inertiajs/inertia-vue3';
+
+defineProps({
+    canLogin: Boolean,
+    canRegister: Boolean,
+    laravelVersion: String,
+    phpVersion: String,
+});
+</script>
+<template>
+    <AppLayout title="Dashboard">
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                Dashboard
+            </h2>
+        </template>
+        <h1>コメントを操作するよ</h1>
+    </AppLayout>
+</template>
+<script>
+export default {
+    name: "Are.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,10 @@ Route::get('/', function () {
     ]);
 });
 
+Route::get('/adminComment', function () {
+    return Inertia::render('Comment', [
+    ]);
+});
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
