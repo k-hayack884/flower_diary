77cf4f712edb171860109eb31968e95b4c8cd125
k-hayack884 <hayack885@gmail.com>
2023-03-30 21:14:48 +0900

    api認証をtokenにする

--- config/auth.php
+++ config/auth.php
@@ -41,8 +41,9 @@ return [
             'provider' => 'users',
         ],
         'api' => [
-            'driver' => 'session',
+            'driver' => 'token',
             'provider' => 'users',
+            'hash' => false,
         ],
     ],
 
