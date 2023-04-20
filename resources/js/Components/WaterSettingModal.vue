f9a6904cfd77d196194b4b41b82fee8f7262a895
k-hayack884 <hayack885@gmail.com>
2023-04-20 19:07:01 +0900

    タグセレクターをどうするか？

--- resources/js/Components/WaterSettingModal.vue
+++ resources/js/Components/WaterSettingModal.vue
@@ -1,35 +1,81 @@
-CheckSeatModal.vue<template>
+CheckSeatModal.vue
+
+
+<template>
 
     <div id="overlay" @click="closeModal()" v-show="isOpen" class="z-20 flex justify-center">
 
-            <div
-                class=" p-8 bg-white w-3/4 lg:py-32 lg:px-16 lg:pl-10 lg:w-1/2 tails-selected-element"
-                contenteditable="true" @click.stop="">
-                <div class="flex flex-col items-start w-full lg:max-w-lg mx-auto"> <!-- mx-autoを追加 -->
-                    <p class="inline-block px-2 py-1 mb-5 font-medium tracking-wider text-gray-900 uppercase bg-gray-200 rounded-full text-xxs">
-                        {{waterSetting}}
-                    </p>
-                    <p class="py-5 mb-5 text-gray-600 lg:text-xl">
-                        <span class="font-bold">    {{waterSetting}}</span> will help you craft and build your next idea.
-                        Utilize our
-                        drag and drop components to build the application of your dreams.
-                    </p>
-                    <div class="flex w-full">
-                        <button type="submit"
-                                class="btn btn-success bg-gradient-to-br from-green-300 to-green-800 hover:bg-gradient-to-tl text-white rounded px-10 my-4 flex-1"
-                                data-rounded="rounded-lg" data-primary="gray-900" @click="redirectToRegister">
-                            新規登録
-                        </button>
-                        <button type="submit"
-                                class="btn btn-success bg-gradient-to-br from-green-300 to-green-800 hover:bg-gradient-to-tl text-white rounded px-10 my-4 flex-1 "
-                                data-rounded="rounded-lg" data-primary="gray-900" @click="redirectToLogin">
-                            ログイン
-                        </button>
+        <div class="p-8 bg-white w-3/4 lg:py-32 lg:px-16 lg:pl-10 lg:w-1/2 tails-selected-element"
+             contenteditable="true" @click.stop="" style="max-height: 120vh; overflow-y: auto;">
+            {{ waterSetting }}
+            <div class="flex flex-col items-start w-full lg:max-w-lg mx-auto"> <!-- mx-autoを追加 -->
+                <div class="grid grid-cols-4 gap-4">
+                    <button v-for="(month, index) in 12" :key="month"
+                            v-if="waterSetting.months"
+                            :class="{'bg-blue-900': waterSetting.months.includes(index+1)}"
+                            class="bg-blue-500  text-white font-bold py-2 px-4 rounded-full"
+                            @click="selectMonth(index)">
+                        {{ month }}月
+                    </button>
+                </div>
+                <div class="grid grid-cols-3 w-full lg:max-w-lg mx-auto">
+                    <div class="btn-group flex justify-center">
+                        <label class="btn px-16" :class="{ 'bg-blue-500': waterSetting.waterAmount === 'a_lot' }">
+                            <input @click="selectAmount('a_lot')" type="radio" name="options" class="hidden"/>
+                            <span style="writing-mode: horizontal-tb;">たっぷり</span>
+                        </label>
                     </div>
+                    <div class="btn-group flex justify-center">
+                        <label class="btn px-16"
+                               :class="{ 'bg-blue-500': waterSetting.waterAmount === 'moderate_amount' }">
+                            <input @click="selectAmount('moderate_amount')" type="radio" name="options" class="hidden"/>
+                            <span style="writing-mode: horizontal-tb;">適量</span>
+                        </label>
+                    </div>
+                    <div class="btn-group flex justify-center">
+                        <label class="btn px-16" :class="{ 'bg-blue-500': waterSetting.waterAmount === 'sparingly' }">
+                            <input @click="selectAmount('sparingly')" type="radio" name="options" class="hidden"/>
+                            <span style="writing-mode: horizontal-tb;">ひかえめ</span>
+                        </label>
+                    </div>
+                </div>
+                <div class="form-control">
+                    <label class="label">
+                        <span class="label-text">水やり間隔</span>
+                    </label>
+                    <label class="input-group">
+                        <input type="text" placeholder="" class="input input-bordered"/>
+                        <span>現在の水やり間隔:{{waterSetting.wateringInterval}}</span>
+                    </label>
+                </div>
+                <div class="form-control">
+                    <label class="label">
+                        <span class="label-text">水やり回数</span>
+                    </label>
+                    <label class="input-group">
+                        <input type="text" placeholder="" class="input input-bordered"/>
+                        <span>現在の水やり回数:{{waterSetting.wateringTimes}}</span>
+                    </label>
+                </div>
+                <div class="form-control">
+                    <label class="label">
+                        <span class="label-text">備考欄</span>
+                    </label>
+                    <textarea class="textarea textarea-success" :placeholder="waterSetting.note"></textarea>
                 </div>
             </div>
         </div>
-
+        <smart-tagz
+            autosuggest
+            editable
+            inputPlaceholder="Select Countries ..."
+            :sources="sources"
+            :allowPaste="{delimiter: ','}"
+            :allowDuplicates="false"
+            :maxTags="20"
+            :defaultTags="['United Kingdom', 'Uruguay', 'Uzbekistan']"
+        />
+    </div>
 </template>
 
 <style>
@@ -49,8 +95,16 @@ CheckSeatModal.vue<template>
 }
 </style>
 <script>
+import { SmartTagz } from "smart-tagz";
+import "smart-tagz/dist/smart-tagz.css";
+
+import { defineComponent } from "vue";
+
 export default {
     name: "WaterSettingModal",
+    components: {
+        SmartTagz,
+    },
     props: {
         openModal: {
             type: Boolean,
@@ -59,7 +113,8 @@ export default {
         waterSetting: {
             type: Object,
             default: () => []
-        }
+        },
+        tags: ['ほげえ']
     },
     data() {
         return {
@@ -76,6 +131,24 @@ export default {
             this.isOpen = false;
             this.$emit("closeModal");
         },
+        selectMonth(index) {
+            const month = index + 1;
+            const indexInMonths = this.waterSetting.months.indexOf(month);
+            if (indexInMonths === -1) {
+                this.waterSetting.months.push(month);
+            } else {
+                this.waterSetting.months.splice(indexInMonths, 1);
+            }
+        },
+        selectAmount(amount) {
+            if (amount === 'a_lot') {
+                this.waterSetting.waterAmount = 'a_lot'
+            } else if (amount === 'moderate_amount') {
+                this.waterSetting.waterAmount = 'moderate_amount'
+            } else if (amount === 'sparingly') {
+                this.waterSetting.waterAmount = 'sparingly'
+            }
+        }
     }
 }
 </script>
