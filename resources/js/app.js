6b58ac2790dad8472a0b79bd8e193ab439617fc4
k-hayack884 <hayack885@gmail.com>
2023-06-03 23:15:40 +0900

    httpの修正

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,16 +9,16 @@ import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
 
 const appName = window.document.getElementsByTagName('title')[0]?.innerText || 'Laravel';
-
-createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
-    setup({ el, app, props, plugin }) {
-        return createApp({ render: () => h(app, props) })
-            .use(plugin)
-            .use(ZiggyVue, Ziggy)
-            .mount(el);
-    },
-});
-
+if(typeof app != 'undefined') {
+    createInertiaApp({
+        title: (title) => `${title} - ${appName}`,
+        resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+        setup({el, app, props, plugin}) {
+            return createApp({render: () => h(app, props)})
+                .use(plugin)
+                .use(ZiggyVue, Ziggy)
+                .mount(el);
+        },
+    });
+}
 InertiaProgress.init({ color: '#4B5563' });
