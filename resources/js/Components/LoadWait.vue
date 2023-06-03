c2710c9d34bbe09ebbbbf2137726d61bd3c7920d
k-hayack884 <hayack885@gmail.com>
2023-06-03 17:58:52 +0900

    実験

---
+++ resources/js/Components/LoadWait.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="loadWait">
+      <div v-if="show" class="z-999"><img src="../../icon/loading_wait.gif" alt=""></div>
+  </div>
+</template>
+
+<script>
+
+export default {
+    el: "#LoadWait",
+  props: {
+    text: {
+      default: "ロード中...",
+      type: String,
+    },
+    show: {
+      default: false,
+      type: Boolean,
+    },
+  },
+
+  template: '<div v-if="show"><img src="../../icon/loading_wait.gif">&nbsp;<span v-text="text"></span></div>'
+};
+</script>
