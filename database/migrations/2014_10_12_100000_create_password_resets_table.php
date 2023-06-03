3ef1fd499bc1e81c3043a94fccfe0331c84f82f0
k-hayack884 <hayack885@gmail.com>
2023-06-03 18:14:01 +0900

    実験

--- database/migrations/2014_10_12_100000_create_password_resets_table.php
+++ database/migrations/2014_10_12_100000_create_password_resets_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('password_resets', function (Blueprint $table) {
-            $table->string('email',191);
+            $table->string('email',150);
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
